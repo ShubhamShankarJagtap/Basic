f3fc12d4ca5b1a8e860e57c2af3f30679799e3fe
ShubhamShankarJagtap <shubh.jagtap14 @gmail.com>
2023-11-28 13:39:04 +0530

    Array

--- demo/src/test/java/com/example/DemoArray.java
+++ demo/src/test/java/com/example/DemoArray.java
@@ -4,11 +4,20 @@ public class DemoArray {
 
     void learn(int[] arr) {
 
+        int sum = 0, average = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum = sum + arr[i];
+            average = sum / arr.length;
+
+        }
+
+        System.out.println("The sum of the given array is : " + sum + " Average of the given array is :" + average);
     }
 
     public static void main(String[] args) {
 
         int[] arr = { 34, 53, 90, 78, 43, 55, 47 };
+        new DemoArray().learn(arr);
 
     }
 }
