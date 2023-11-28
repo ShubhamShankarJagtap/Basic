703f819f43c692bdc0a565455dcc165f2a62a751
ShubhamShankarJagtap <shubh.jagtap14 @gmail.com>
2023-11-28 19:47:37 +0530

    Method OverLoading

--- demo/src/test/java/com/example/Basic.java
+++ demo/src/test/java/com/example/Basic.java
@@ -1,5 +1,38 @@
 package com.example;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class Basic {
 
+    String path = ".//Feature//Demo.Feature";
+    FileInputStream inputStream;
+
+    void run() {
+        System.out.println(" I am running");
+    }
+
+    void run(int a) {
+        System.out.println(" i am running at a speend of " + a + "MPH");
+    }
+
+    void run(String s) throws IOException {
+        inputStream = new FileInputStream(path);
+        int character = inputStream.read();
+
+        while (character != -1) {
+
+            char c = (char) character;
+            System.out.println(c);
+            inputStream.close();
+        }
+        System.out.println(s);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Basic foo = new Basic();
+        foo.run();
+        foo.run(10);
+        foo.run(" This is the String representation of run.");
+    }
 }
